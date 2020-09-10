601fcae78ecd46285ea64c5550c3e9085ed9ab22
DenisAleshkov <aleshkov592@gmail.com>
2020-09-10 20:46:52 +0300

    end the Pie

--- src/components/Chart/Pie.jsx
+++ src/components/Chart/Pie.jsx
@@ -1,40 +1,58 @@
 import React from 'react'
 import { Pie } from 'react-chartjs-2'
+import './Chart.css'
 const PieComponent = ({ languages }) => {
  
   const labels = Object.keys(languages)
   const valueLanguage = Object.values(languages)
-    .map( (item) => { return item.value  } )
+    .map( (item) => { return item.value } )
     
   console.log('languages',valueLanguage)
  
  
 const data = {
+  
     labels: labels,
+   
     datasets: [{
       data: valueLanguage,
       backgroundColor: [
-      '#FF6384',
-      '#36A2EB',
-      '#FFCE56',
-      '#FF6384',
-      '#36A2EB',
-      '#FFCE56',
-      ],
-      hoverBackgroundColor: [
-      '#FF6384',
-      '#36A2EB',
-      '#FFCE56'
+        '#FF6384',
+        '#36A2EB',
+        '#FFCE56',
+        '#3ADFD1',
+        '#ED6868',
+        '#BEDB4C',
+        '#D331C9',
+        '#E17B37',
+        '#51164F',
+        '#497373'
       ]
-    }]
+    }],
   };
-  
+  const options = {
+    responsive: false,
+    legend: {
+      labels: {
+        boxWidth: 20
+      }
+    }
+  }
  
     return(
-        <div className="pie">
-           <Pie width={100}
-                height={50} 
-                data = { data } />
+        <div className="pie" style={
+          {
+            height: '500px', 
+            width: '500px', 
+           
+          }}>
+           <Pie 
+                data = { data }
+                width = {450}
+                height = {450}
+                options = {options }
+                />
+              
         </div>
     )
 }
