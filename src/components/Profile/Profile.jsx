1fae18d033431494d977fa979fb5d5eacdd484ff
DenisAleshkov <aleshkov592@gmail.com>
2020-09-29 20:52:46 +0300

    recod in progile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,10 +11,10 @@ const Profile = () => {
     function daysInMonth (month, year) {
         return new Date(year, month, 0).getDate();
     }
-    
 
     const date = new Date()
-    const events = activity.map((a) => {
+   
+     const events = activity.map((a) => {
         if (date.getDate() - 7 <= new Date(a.created_at).getDate()
             && date.getMonth() <= new Date(a.created_at).getMonth()) {
             return (
@@ -23,8 +23,7 @@ const Profile = () => {
             )
         }
         else{
-            console.log(daysInMonth(8,2020))
-            if(daysInMonth(8,2020) + date.getDate() - 7 <= new Date(a.created_at).getDate()
+            if(daysInMonth(date.getMonth(), date.getFullYear()) + date.getDate() - 7 <= new Date(a.created_at).getDate()
             && date.getMonth() <= new Date(a.created_at).getMonth()+1){
                 return (
                     <Events key={a.id} name={a.repo.name} date={a.created_at}
