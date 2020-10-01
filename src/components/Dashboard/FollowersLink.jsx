4411ef5e4be1748cd2b1211a7e572f27be08c052
DenisAleshkov <aleshkov592@gmail.com>
2020-10-01 21:32:40 +0300

    added finish followers

--- src/components/Dashboard/FollowersLink.jsx
+++ src/components/Dashboard/FollowersLink.jsx
@@ -2,15 +2,16 @@ import React, { } from "react"
 import { Card } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
+
 const FollowersLink = ({followers}) => {
-console.log(followers)
+
  return(
      <LinkContainer to="/followers">
         <Card style={{ width: '18rem'}}>
         <Card.Body style={{ cursor: 'pointer'}}>
             <Card.Title>Followers</Card.Title>
             <Card.Text>
-              fdsf
+               { followers.length }
             </Card.Text>
         </Card.Body>
     </Card>
--- src/components/Follow/Followers.jsx
+++ src/components/Follow/Followers.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
+import { GithubContext } from './../../context/context'
+import './Followers.css'
 
 const Followers = () => {
+    const { followers } = React.useContext(GithubContext)
+    console.log(followers)
+
+    const followersList = followers.map((f, i) => {
+        return (
+            <div className="follower" key={f.node_id}>
+                <img className="follower-image" src={f.avatar_url} />
+                <span className="follower-title">{f.login}</span>
+            </div>
+        )
+    })
+
     return (
-        <>Followers</>
+        <div className="follower-list main">
+            { followersList}
+        </div>
     )
 }
 
